ece706f3b023f96639c8a13a8201c4fa74ac21c1
Yahya <yhya9813@gmail.com>
2023-02-16 13:50:09 +0300

    chore: rewrite response message in arabic

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -10,9 +10,9 @@ class CourseController extends Controller
 {
     public function index($course)
     {
-        if (!preg_match('/^[a-zA-Z]{1,5}-[0-9]{1,5}$/', $course)) return response()->json(['message' => 'Wrong course format']);
+        if (!preg_match('/^[a-zA-Z]{1,5}-[0-9]{1,5}$/', $course)) return response()->json(['message' => 'صغية المادة غير صحيحة']);
 
         $course = explode('-', $course);
-        return Courses::with('lectures.classes')->where('course', $course[0])->where('number', $course[1])->first() ?? response()->json(['message' => 'Course not found']);
+        return Courses::with('lectures.classes')->where('course', $course[0])->where('number', $course[1])->first() ?? response()->json(['message' => 'المادة ليست موجودة في سجلاتنا لإضافة المادة تواصل معنا على تويتر']);
     }
 }
