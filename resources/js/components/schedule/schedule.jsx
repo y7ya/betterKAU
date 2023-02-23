5071704a1a635b50847a247ca38a72e5962cc38a
Yahya <yhya9813@gmail.com>
2023-02-23 16:17:04 +0300

    refactor: extract transformation functions to utils.js file

--- resources/js/components/schedule/schedule.jsx
+++ resources/js/components/schedule/schedule.jsx
@@ -15,58 +15,11 @@ import {
     AppointmentTooltip,
 } from "@devexpress/dx-react-scheduler-material-ui";
 import {theme} from './theme'
+import {courseToAppointment} from '../../utils.js';
 
 const Schedule = ({ selectedCourses }) => {
     const currentDate = "1337-02-03";
-
-    const dayletterToNumber = (dayLetter) => {
-        switch (dayLetter) {
-            case "U": // sunday
-                return "3";
-            case "M": // monday
-                return "4";
-            case "T": // tuesday
-                return "5";
-            case "W": // wednesday
-                return "6";
-            case "R": // thursday
-                return "7";
-            case "F": // friday
-                return "8";
-            case "S": // saturday
-                return "9";
-
-            default:
-                break;
-        }
-    };
-
-    const courseToAppointment = () => {
-        if (!selectedCourses?.length) return [];
-        let courseData = [];
-        selectedCourses.forEach((course) => {
-            let classData = course.lecture["classes"].map((classs) => {
-                return {
-                    id: classs.id,
-                    lecture_id: course.lecture.id,
-                    title: `${course.course}-${course.number} | ${classs.lecturer}`,
-                    course: "",
-                    startDate: `1337-02-0${dayletterToNumber(classs.day)}T${
-                        classs.time_start
-                    }`,
-                    endDate: `1337-02-0${dayletterToNumber(classs.day)}T${
-                        classs.time_end
-                    }`,
-                    location: `Building ${course.building} | Room ${course.room}`,
-                    color:course.color
-                };
-            });
-            courseData.push(...classData);
-        });
-        return courseData;
-    };
-
-    let schedulerData = [...courseToAppointment()];
+    let schedulerData = [...courseToAppointment(selectedCourses)];
 
     const checkForSimilarties = (schedulerData) => {
         let NoOverlapping = schedulerData;
---
+++ resources/js/utils.js
@@ -0,0 +1,46 @@
+export const dayletterToNumber = (dayLetter) => {
+    switch (dayLetter) {
+        case "U": // sunday
+            return "3";
+        case "M": // monday
+            return "4";
+        case "T": // tuesday
+            return "5";
+        case "W": // wednesday
+            return "6";
+        case "R": // thursday
+            return "7";
+        case "F": // friday
+            return "8";
+        case "S": // saturday
+            return "9";
+
+        default:
+            break;
+    }
+};
+
+export const courseToAppointment = (selectedCourses) => {
+    if (!selectedCourses?.length) return [];
+    let courseData = [];
+    selectedCourses.forEach((course) => {
+        let classData = course.lecture["classes"].map((classs) => {
+            return {
+                id: classs.id,
+                lecture_id: course.lecture.id,
+                title: `${course.course}-${course.number} | ${classs.lecturer}`,
+                course: "",
+                startDate: `1337-02-0${dayletterToNumber(classs.day)}T${
+                    classs.time_start
+                }`,
+                endDate: `1337-02-0${dayletterToNumber(classs.day)}T${
+                    classs.time_end
+                }`,
+                location: `Building ${course.building} | Room ${course.room}`,
+                color:course.color
+            };
+        });
+        courseData.push(...classData);
+    });
+    return courseData;
+};
