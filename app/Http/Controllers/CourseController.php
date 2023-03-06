ca3ac03eb46bf2072e3424190a146dc66d79eabc
Yahya <yhya9813@gmail.com>
2023-03-07 00:20:54 +0300

    fix: get course data of active term

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Courses;
+use App\Models\Terms;
 use Exception;
 use Illuminate\Http\Request;
 
@@ -11,9 +12,9 @@ class CourseController extends Controller
     public function index(Request $request)
     {
         if (!preg_match('/^[a-zA-Z]{1,5}-[0-9]{1,5}$/', $request->course)) return response()->json(['message' => 'صغية المادة غير صحيحة']);
-
+        
         $course = explode('-', $request->course);
-        if(!Courses::where('course', $course[0])->first()) return response()->json(['message' => 'المادة ليست موجودة في سجلاتنا لإضافة المادة تواصل معنا على تويتر']); 
-        return Courses::with(['term','lectures.classes'])->where('course', $course[0])->where('number', $course[1])->first() ?? response()->json(['message' => 'لا توجد شعب لهذه المادة']);
+        if(!Courses::where('term_id',Terms::where('active',1)->first()->id)->where('course', $course[0])->first()) return response()->json(['message' => 'المادة ليست موجودة في سجلاتنا لإضافة المادة تواصل معنا على تويتر']); 
+        return Courses::with(['term','lectures.classes'])->where('term_id',Terms::where('active',1)->first()->id)->where('course', $course[0])->where('number', $course[1])->first() ?? response()->json(['message' => 'لا توجد شعب لهذه المادة']);
     }
 }
