859fc780548d47ded52f4f50ea1483766fcd9e51
Yahya <yhya9813@gmail.com>
2023-03-08 01:44:07 +0300

    feat: log course search activities

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Courses;
+use App\Models\Last_activitie;
 use App\Models\Terms;
 use Exception;
 use Illuminate\Http\Request;
@@ -11,8 +12,10 @@ class CourseController extends Controller
 {
     public function index(Request $request)
     {
+        Last_activitie::create(['title'=>'Search attempt','activity'=>str_split($request->course, 255)[0]]);
+
         if (!preg_match('/^[a-zA-Z]{1,5}-[0-9]{1,5}$/', $request->course)) return response()->json(['message' => 'صغية المادة غير صحيحة']);
-        
+   
         $course = explode('-', $request->course);
         if(!Courses::where('term_id',Terms::where('active',1)->first()->id)->where('course', $course[0])->first()) return response()->json(['message' => 'المادة ليست موجودة في سجلاتنا لإضافة المادة تواصل معنا على تويتر']); 
         return Courses::with(['term','lectures.classes'])->where('term_id',Terms::where('active',1)->first()->id)->where('course', $course[0])->where('number', $course[1])->first() ?? response()->json(['message' => 'لا توجد شعب لهذه المادة']);
---
+++ database/migrations/2023_03_07_222455_create_last_activities_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('last_activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255)->nullable();
+            $table->string('activity', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('last_activities');
+    }
+};
