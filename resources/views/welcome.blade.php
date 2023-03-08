438f574af550e11854c8717e68d94ad99836f0d9
Yahya <yhya9813@gmail.com>
2023-03-09 00:13:15 +0300

    update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="بسطنا لطالب جامعة الملك عبدالعزيز تنظيم الجدول الدراسي الان بطريقة سهلة وفعالة">
+    <meta name="description" content="انشئ جدولك بكل سهولة">
 
     <title>betterKAU | جدول</title>
     <link rel="icon" type="image/png" href="{{ Vite::asset('resources/imgs/favicon-32x32.png') }}" sizes="32x32" />
@@ -13,6 +13,15 @@
     @viteReactRefresh
     @vite('resources/js/app.jsx')
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-HM7QGWFJ21"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HM7QGWFJ21');
+</script>
 
 <body>
     <div id="app"></div>
