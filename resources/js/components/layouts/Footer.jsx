eb40e3cc8d5d491801277a08c9ec10ebe6a25115
Yahya <yhya9813@gmail.com>
2023-03-10 04:51:24 +0300

    style: add twitter accounts links to footer

--- resources/js/components/layouts/Footer.jsx
+++ resources/js/components/layouts/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Footer = () => <footer className="page-footer font-small">
     <div className="footer-copyright text-center pt-2 footer-color" dir="rtl">
-    تمت برمجة منصة <a className="text-white text-decoration-none" href="">BetterKAU</a> من قبل <a className="text-white text-decoration-none" href="">يحيى</a> و <a className="text-white text-decoration-none" href="">مالك</a>
+    تمت برمجة منصة <a className="text-white text-decoration-none" href="https://twitter.com/betterkau" target="_blank">BetterKAU</a> من قبل <a className="text-white text-decoration-none" href="https://twitter.com/imy7ya" target="_blank">يحيى</a> و <a className="text-white text-decoration-none" href="https://twitter.com/MalekX0_" target="_blank">مالك</a>
     </div>
 </footer>
 
